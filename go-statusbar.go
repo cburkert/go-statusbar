47ff444d91f46b89f40edf730bd8e5dcafebc4b3
Christian Burkert <post@cburkert.de>
2015-03-07 22:43:57 +0100

    Use creator function for DateReporter

--- go-statusbar.go
+++ go-statusbar.go
@@ -9,6 +9,6 @@ func main() {
 	statusBar := NewStatusBar(" | ")
 	statusBar.AddReporter(volume.NewVolumeReporter())
 	statusBar.AddReporter(battery.NewPowerReporter("/sys/class/power_supply/"))
-	statusBar.AddReporter(&DateReporter{"Mon 02 Ý 15:04"})
+	statusBar.AddReporter(NewDateReporter("Mon 02 Ý 15:04"))
 	statusBar.Run()
 }
